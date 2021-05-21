703737487860aceed33b54b74ae5e8d23c7af91e
Rahul <rahulkumarsinha5316@gmail.com>
2021-05-21 19:56:08 +0530

    MediaRecorder Functionalities

--- activity/script.js
+++ activity/script.js
@@ -4,7 +4,17 @@
 
 let videoPlayer = document.querySelector("video");
 
-let constraints = {video:true};
+let recordedButton = document.querySelector("#record-video");
+
+let photoButton = document.querySelector("#capture-photo");
+
+let recordingState = false;
+ 
+
+let constraints ={video:true};
+
+let recordedData;
+let mediaRecorder;
 
 (async function(){
 
@@ -12,11 +22,78 @@ let constraints = {video:true};
  // the above function gives the number of devices connected to the device
 
  //console.log(devices);
-  let mediastream = await navigator.mediaDevices.getUserMedia(constraints);
-  console.log(mediastream);
+  // let mediastream = await navigator.mediaDevices.getUserMedia(constraints);
+  // console.log(mediastream);
 
-  videoPlayer.srcObject = mediastream;
-  //finds the video camera
+  // videoPlayer.srcObject = mediastream;
+  // //finds the video camera
+
+try{
+   let mediastream = await navigator.mediaDevices.getUserMedia(constraints);
+
+   videoPlayer.srcObject = mediastream;
+
+   mediaRecorder = new mediaRecorder(mediastream);
+
+   mediaRecorder.onstop = function(e){
+
+    console.log("Inside on stop");
+    console.log(e);
+   }
+
+
+   mediaRecorder.onstart = function(e) {
+     console.log("Inside on start");
+
+     console.log(e);
+   }
+
+
+   mediaRecorder.ondataavailable = function(e) {
+
+    console.log("Inside on data available");
+
+    recordedData = e.data;
+
+
+   }
+
+   console.log(mediaRecorder);
+
+   recordedButton.addEventListener("click", function(){
+     
+    if(recordingState) {
+
+      mediaRecorder.stop();
+
+      recordedButton.innerHTML = "Record";
+    }
+    else {
+
+      mediaRecorder.start();
+
+      recordedButton.innerHTML = "Recording";
+    }
+
+    recordingState = !recordedButton;
+
+
+   })
+
+
+
+
+}
+
+catch(error){
+
+}
+
+
+
+
+
+
 
 
 })();
