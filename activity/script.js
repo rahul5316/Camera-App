29f9d910148e6c807373799a57e3dc5b89ec2863
Rahul <rahulkumarsinha5316@gmail.com>
2021-05-21 22:09:02 +0530

    Captures and Downloads photos and Videos

--- activity/script.js
+++ activity/script.js
@@ -9,60 +9,62 @@ let recordedButton = document.querySelector("#record-video");
 let photoButton = document.querySelector("#capture-photo");
 
 let recordingState = false;
- 
 
-let constraints ={video:true};
+
+let constraints = { video: true };
 
 let recordedData;
 let mediaRecorder;
 
-(async function(){
+(async function () {
 
- //let devices =  await navigator.mediaDevices.enumerateDevices();
- // the above function gives the number of devices connected to the device
+  //let devices =  await navigator.mediaDevices.enumerateDevices();
+  // the above function gives the number of devices connected to the device
 
- //console.log(devices);
+  //console.log(devices);
   // let mediastream = await navigator.mediaDevices.getUserMedia(constraints);
   // console.log(mediastream);
 
   // videoPlayer.srcObject = mediastream;
   // //finds the video camera
 
-try{
-   let mediastream = await navigator.mediaDevices.getUserMedia(constraints);
 
-   videoPlayer.srcObject = mediastream;
+  let mediastream = await navigator.mediaDevices.getUserMedia(constraints);
 
-   mediaRecorder = new MediaRecorder(mediastream);
+  videoPlayer.srcObject = mediastream;
 
-   mediaRecorder.onstop = function(e){
+  mediaRecorder = new MediaRecorder(mediastream);
+
+  mediaRecorder.onstop = function (e) {
 
     console.log("Inside on stop");
     console.log(e);
-   }
+  }
 
 
-   mediaRecorder.onstart = function(e) {
-     console.log("Inside on start");
+  mediaRecorder.onstart = function (e) {
+    console.log("Inside on start");
 
-     console.log(e);
-   }
+    console.log(e);
+  }
 
 
-   mediaRecorder.ondataavailable = function(e) {
+  mediaRecorder.ondataavailable = function (e) {
 
     console.log("Inside on data available");
 
     recordedData = e.data;
 
+    saveVideoToFs();
 
-   }
 
-   console.log(mediaRecorder);
+  }
 
-   recordedButton.addEventListener("click", function(){
-     
-    if(recordingState) {
+  console.log(mediaRecorder);
+
+  recordedButton.addEventListener("click", function () {
+
+    if (recordingState) {
 
       mediaRecorder.stop();
 
@@ -75,27 +77,58 @@ try{
       recordedButton.innerHTML = "Recording";
     }
 
-    recordingState = !recordedButton;
+    recordingState = !recordingState;
 
 
-   })
+  })
 
 
-
-
-}
-
-catch(error){
-
-}
-
-
-
-
-
+  photoButton.addEventListener("click", capturePhotos);
 
 
 
 })();
+
+
+function saveVideoToFs() {
+
+  console.log("Saving Video");
+
+  let videoUrl = URL.createObjectURL(recordedData); //converts blob data to Url
+
+  console.log(videoUrl);
+
+  let aTag = document.createElement("a");
+
+  aTag.download = "video.mp4";
+
+  aTag.href = videoUrl;
+
+  aTag.click();
+  aTag.remove();
+}
+
+
+function capturePhotos() {
+
+  let canvas = document.createElement("canvas");
+
+  canvas.height = videoPlayer.videoHeight;
+
+  canvas.width = videoPlayer.videoWidth;
+
+  let ctx = canvas.getContext("2d");
+
+  ctx.drawImage(videoPlayer, 0, 0);
+
+  let imageUrl = canvas.toDataURL("image/jpg");
+  //canvas object => file url string
+
+  let aTag = document.createElement("a");
+
+  aTag.download = "photo,jpg";
+  aTag.href = imageUrl;
+  aTag.click();
+}
 
 
